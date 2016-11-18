2c0b033f4c020e5e17b9f9ae5ffd9c6b051f5ece
Tom Gray <tomg77@yahoo.co.uk>
2016-11-18 14:07:20 +0000

    Added function to get the words from the dictionary

--- cipher_crack/main.py
+++ cipher_crack/main.py
@@ -1,11 +1,20 @@
 import argparse
+from cipher_crack.ciphers import transposition
 
-def main(args)
+def main(args):
     parser = argparse.ArgumentParser(description="Crack ciphers\
             based on a dictionary attack")
-    parser.add_argument("cipher",option="store",type="str")
-    parser.add_argument("--dict",option="store",type="str",
+    parser.add_argument("cipher",action="store",type=str)
+    parser.add_argument("--dict",action="store",type=str,
             default="/usr/share/dict/cracklib-small")
-
     results = parser.parse_args(args)
+
+    words = get_words_from_file(results.dict)
+    if results.cipher == "transposition":
+        print(words)
+
+def get_words_from_file(location):
+    return [(word.rstrip('\n')).replace("'","") for word in open(location,'r')]
+
+def crack(dec_func,dictionary):
 
