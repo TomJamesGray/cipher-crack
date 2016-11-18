60edc25811d02d2fa2fe06cf94182e5c3c253505
Tom Gray <tomg77@yahoo.co.uk>
2016-11-18 17:42:57 +0000

    Added cipher text option

--- cipher_crack/main.py
+++ cipher_crack/main.py
@@ -1,20 +1,25 @@
 import argparse
+import logging
 from cipher_crack.ciphers import transposition
 
 def main(args):
     parser = argparse.ArgumentParser(description="Crack ciphers\
             based on a dictionary attack")
     parser.add_argument("cipher",action="store",type=str)
+    parser.add_argument("cipher_text",action="store",type=str)
     parser.add_argument("--dict",action="store",type=str,
             default="/usr/share/dict/cracklib-small")
     results = parser.parse_args(args)
 
     words = get_words_from_file(results.dict)
     if results.cipher == "transposition":
-        print(words)
+        crack(transposition.decipher,words,results.split(","))
 
 def get_words_from_file(location):
     return [(word.rstrip('\n')).replace("'","") for word in open(location,'r')]
 
-def crack(dec_func,dictionary):
+def crack(cipher_txt,dec_func,dictionary,likely_words):
+    if not callable(dec_func):
+        logging.error("Decipher function is not callable")
+        raise ValueError("dec_func is not callable")
 
