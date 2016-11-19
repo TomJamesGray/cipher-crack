aa338744f9b84fc1cf5b8b1efd668c29e99192cd
TomJamesGray <tomg77@yahoo.co.uk>
2016-11-19 18:12:33 +0000

    Moved crack to a new file and added necessary logging config for it

---
+++ cipher_crack/crack.py
@@ -0,0 +1,20 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def crack(cipher_txt,dec_func,dictionary,likely_words):
+    likely_words = likely_words.split(" ")
+    if not callable(dec_func):
+        logger.error("Decipher function is not callable")
+        raise ValueError("dec_func is not callable")
+
+    for word in dictionary:
+        deciphered = dec_func(cipher_txt,word)
+        if are_words_in_cipher(likely_words,deciphered):
+            logger.info("Key: {} \n{}".format(word,
+                deciphered))
+
+def are_words_in_cipher(words,cipher):
+    for word in words:
+        if word in cipher:
+            return True
--- cipher_crack/main.py
+++ cipher_crack/main.py
@@ -2,11 +2,13 @@ import argparse
 import logging
 import logging.config
 from cipher_crack.ciphers import transposition
+from cipher_crack import crack
 
 logging_config = {
     "version":1,
     "formatters":{
-        "main":{"format":"%(name)s-%(lineno)d: %(message)s"}
+        "main":{"format":"%(name)s-%(lineno)d: %(message)s"},
+        "simple":{"format":"%(message)s"}
     },
     "handlers":{
         "ciphers":{"class":"logging.StreamHandler",
@@ -14,12 +16,18 @@ logging_config = {
             "level":"DEBUG"},
         "user":{"class":"logging.StreamHandler",
             "formatter":"main",
+            "level":"INFO"},
+        "clean":{"class":"logging.StreamHandler",
+            "formatter":"simple",
             "level":"INFO"}
     },
     "loggers":{
         "cipher_crack.ciphers":{
             "handlers":["ciphers"],
             "level":"ERROR"},
+        "cipher_crack.crack":{
+            "handlers":["clean"],
+            "level":"INFO"},
         "cipher_crack.main":{
             "handlers":["user"],
             "level":"INFO"}
@@ -34,21 +42,15 @@ def main(args):
             based on a dictionary attack")
     parser.add_argument("cipher",action="store",type=str)
     parser.add_argument("cipher_text",action="store",type=str)
+    parser.add_argument("likely_words",action="store",type=str)
     parser.add_argument("--dict",action="store",type=str,
             default="/usr/share/dict/cracklib-small")
     results = parser.parse_args(args)
 
     words = get_words_from_file(results.dict)
     if results.cipher == "transposition":
-        crack(results.cipher_text,transposition.decipher,words)
+        crack.crack(results.cipher_text,transposition.decipher,words,results.likely_words)
 
 def get_words_from_file(location):
     return [(word.rstrip('\n')).replace("'","") for word in open(location,'r')]
-
-def crack(cipher_txt,dec_func,dictionary):
-    if not callable(dec_func):
-        logger.error("Decipher function is not callable")
-        raise ValueError("dec_func is not callable")
-    for word in dictionary:
-        print(dec_func(cipher_txt,word))
 
