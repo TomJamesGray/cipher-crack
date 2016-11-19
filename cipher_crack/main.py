106c4e5f462a351da91bd0c5d57d4d0667b098ea
TomJamesGray <tomg77@yahoo.co.uk>
2016-11-19 16:58:17 +0000

    Made crack acutally do something, ie run the decipher function for each word in the provided dictionary

--- cipher_crack/main.py
+++ cipher_crack/main.py
@@ -3,6 +3,7 @@ import logging
 from cipher_crack.ciphers import transposition
 
 def main(args):
+    logging.basicConfig(level=logging.DEBUG)
     parser = argparse.ArgumentParser(description="Crack ciphers\
             based on a dictionary attack")
     parser.add_argument("cipher",action="store",type=str)
@@ -13,13 +14,15 @@ def main(args):
 
     words = get_words_from_file(results.dict)
     if results.cipher == "transposition":
-        crack(transposition.decipher,words,results.split(","))
+        crack(results.cipher_text,transposition.decipher,words)
 
 def get_words_from_file(location):
     return [(word.rstrip('\n')).replace("'","") for word in open(location,'r')]
 
-def crack(cipher_txt,dec_func,dictionary,likely_words):
+def crack(cipher_txt,dec_func,dictionary):
     if not callable(dec_func):
         logging.error("Decipher function is not callable")
         raise ValueError("dec_func is not callable")
+    for word in dictionary:
+        print(dec_func(cipher_txt,word))
 
