8a1869ec2fdc4a8b7d759cceb9dc6dea62a62778
Tom Gray <tomg77@yahoo.co.uk>
2016-11-29 10:13:00 +0000

    Added logging support and exception handling to dictionary functions£

--- cipher_crack/dictionary_functions.py
+++ cipher_crack/dictionary_functions.py
@@ -1,7 +1,15 @@
 import string
+import logging
 from itertools import combinations
 
+logger = logging.getLogger(__name__)
+
 def length_words(length):
+    try:
+        length = int(length)
+    except ValueError as e:
+        logger.exception(e)
+        return False
     for combination in combinations(string.ascii_lowercase,length):
         #Flatten and yield the tupple
         yield ''.join(combination)
