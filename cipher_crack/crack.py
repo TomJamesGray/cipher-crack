d532f7472b31126b6e9e67f1c7a1a28056191378
TomJamesGray <tomg77@yahoo.co.uk>
2016-12-28 15:59:39 +0000

    Implemented ranking results from the count of likely words

--- cipher_crack/crack.py
+++ cipher_crack/crack.py
@@ -13,15 +13,23 @@ def crack(cipher_txt,dec_func,dictionary,likely_words):
         logger.error("Decipher function is not callable")
         raise ValueError("dec_func is not callable")
 
+    results = []
     for word in dictionary():
         deciphered = dec_func(cipher_txt,word)
         if deciphered != None:
-            if are_words_in_cipher(likely_words,deciphered):
-                logger.info("Key: {} \n{}".format(word,
-                    deciphered))
+            score = count_words_in_cipher(likely_words,deciphered)
+            if score != 0:
+                results.append((word,deciphered,score))
 
-def are_words_in_cipher(words,cipher):
+    #Sort results by the score
+    results = sorted(results,key=lambda x:x[2])
+    for result in results:
+        print("Score: {},Key: {}\n{}".format(result[2],result[0],result[1]))
+
+def count_words_in_cipher(words,cipher):
     cipher = cipher.upper()
+    count = 0
     for word in words:
         if word.upper() in cipher:
-            return True
+            count += 1
+    return count
