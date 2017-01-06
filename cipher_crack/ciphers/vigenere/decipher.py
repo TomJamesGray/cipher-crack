0df0c6e74129fd1676f8cf64bf4fa74be9bf7dc3
TomJamesGray <tomg77@yahoo.co.uk>
2017-01-06 17:13:24 +0000

    Added vigenere cipher

--- cipher_crack/ciphers/vigenere/decipher.py
+++ cipher_crack/ciphers/vigenere/decipher.py
@@ -3,4 +3,34 @@ import string
 
 logger = logging.getLogger(__name__)
 def decipher(cipher_txt,key):
-    return True
+    """
+    Deciphers a given cipher text using a key with the vigenere cipher
+    """
+    alphabet = string.ascii_lowercase
+    cipher_txt,key = text_to_pos(cipher_txt.lower()),text_to_pos(key.lower())
+    logger.info("Cipher text: {}".format(cipher_txt))
+    logger.info("Key: {}".format(key))
+    if cipher_txt == False or key == False:
+        return False
+    out = ""
+    i = 0
+    for c in cipher_txt:
+        if i >= len(key):
+            i = 0
+        out += string.ascii_lowercase[(c-key[i])%26]
+        i += 1
+    return out
+
+def text_to_pos(txt,alphabet=string.ascii_lowercase):
+    """
+    Converts a string into the positions in a string, by default the
+    ascii lowercase alphabet, returning a list
+    """
+    out = []
+    for c in txt:
+        try:
+            out.append(alphabet.index(c))
+        except ValueError:
+            logger.error("Character {} not in alphabet {}".format(c,alphabet))
+            return False
+    return out
